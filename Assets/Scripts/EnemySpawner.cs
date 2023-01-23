6cb4d7b479f616c1400acc5a92949f1c5c2198e4
Ruan Barroso <ruan.x14@gmail.com>
2023-01-22 21:37:34 -0400

    coroutine added to replace spawn method by time

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -10,37 +10,54 @@ public class EnemySpawner : MonoBehaviour
 
 
     public float initialDelay;
-    public float delayBetween;
+    //public float delayBetween; //Not used anymore
+    public float distanceMinSpawn = 5.5f;
+    public float distanceMaxSpawn = 9f;
 
-    public float dinoMinX, dinoMaxX;
+    public float dinoFlyMinY = 0, dinoFlyMaxY = 1.5f;
     public Dino dino;
     private void Start()
     {
-        InvokeRepeating("GenerateEnemy", initialDelay, delayBetween);
+
+        //InvokeRepeating("GenerateEnemy", initialDelay, delayBetween);
+        StartCoroutine(GenerateEnemy());
     }
 
     // Update is called once per frame
-    private void GenerateEnemy()
+    private IEnumerator GenerateEnemy()
     {
-        int controller = Random.Range(1, 7);
-        if(dino.score >= 300 && controller <=2)
-        {
-            var positionRandomY = Random.Range(dinoMinX, dinoMaxX);
-            Vector3 position = new Vector3(
-                transform.position.x,
-                transform.position.y + positionRandomY,
-                transform.position.z
-                );
-            Instantiate(dinoFlyPrefab, position, Quaternion.identity);
+        yield return new WaitForSeconds(initialDelay);
+        GameObject lastEnemySpawned = null;
+        var distanceEnemy = 0f;
+        while (true) {
+            var createEnemyController = lastEnemySpawned == null || Vector3.Distance(transform.position, lastEnemySpawned.transform.position) >= distanceEnemy;
+
+            if (createEnemyController)
+            {
+                int controller = Random.Range(1, 7);
+                if (dino.score >= 10 && controller <= 2)
+                {
+                    var positionRandomY = Random.Range(dinoFlyMinY, dinoFlyMaxY);
+                    Vector3 position = new Vector3(
+                        transform.position.x,
+                        transform.position.y + positionRandomY,
+                        transform.position.z
+                        );
+                    lastEnemySpawned = Instantiate(dinoFlyPrefab, position, Quaternion.identity);
+                    
+                }
+                else
+                {
+                    int cactiQuantity = cactiThinPrefabs.Length;
+                    int randomCacti = Random.Range(0, cactiQuantity);
+                    var cactiThinPrefab = cactiThinPrefabs[randomCacti];
+                    lastEnemySpawned = Instantiate(cactiThinPrefab, transform.position, Quaternion.identity);
+                }
+                distanceEnemy = Random.Range(distanceMinSpawn, distanceMaxSpawn);
+            }
+            yield return null;
+            //yield return new WaitForSeconds(delayBetween);
         }
-        else
-        {
-            int cactiQuantity = cactiThinPrefabs.Length;
-            int randomCacti = Random.Range(0, cactiQuantity);
-            var cactiThinPrefab = cactiThinPrefabs[randomCacti];
-            Instantiate(cactiThinPrefab, transform.position, Quaternion.identity);
-        }
-        
     }
 
 }
